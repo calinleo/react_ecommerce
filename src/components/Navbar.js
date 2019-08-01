c9277be5db4852c4c77aa0f08ee0eab432f0854b
Calin <calinleordean@gmail.com>
2019-08-01 11:01:23 -0400

    Moved button style to own file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import logo from '../logo.svg';
 import styled from 'styled-components';
+import {ButtonContainer} from './Button';
 
 export default class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
                 <Link to="/">
                     <img src={logo} alt="Store" className="navbar-brand" />
                 </Link>
@@ -18,33 +19,21 @@ export default class Navbar extends Component {
                 <Link to="/cart" className="ml-auto">
                     <ButtonContainer>
                         <span className="mr-2">
-                        <i className="fas fa-cart-plus"></i>
+                        <i className="fas fa-shopping-cart"></i>
                         </span>
                         
                         My Cart
                     </ButtonContainer>
                 </Link>
-            </nav>
+            </NavWrapper>
         )
     }
 }
-
-const ButtonContainer = styled.button`
-text-transform:capitalize;
-font-size:1.3rem;
-background:transparent;
-border:0.05rem solid var(--lightBlue);
-color:var(--lightBlue);
-border-radius:0.3rem;
-padding:0.2rem 0.5rem;
-cursor:pointer;
-margin:0.2rem 0.5rem 0.2rem 0;
-transition:all 0.4s ease-in-out;
-&:hover{
-    background: var(--lightBlue);
-    color: var(--mainBlue);
+const NavWrapper = styled.nav`
+background:var(--mainBlue);
+.nav-link{
+    color:var(--mainWhite)!important;
+    font-size:1.3rem;
+    text-transform:capitalize;
 }
-&:focus{
-    outline:none;
-}
-`
+`;
